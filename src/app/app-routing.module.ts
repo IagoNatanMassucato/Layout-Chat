c01d65e4b2b848119822cbc22e7aae883addf766
Iago Natan Massucato de Menezes <iago.menezes@etec.sp.gov.br>
2021-10-29 18:37:11 -0300

    Só o layout não tem função

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,18 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  },
+  {
     path: '',
-    redirectTo: 'folder/Inbox',
+    redirectTo: 'home',
     pathMatch: 'full'
   },
   {
-    path: 'folder/:id',
-    loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
-  }
+    path: 'listagem',
+    loadChildren: () => import('./listagem/listagem.module').then( m => m.ListagemPageModule)
+  },
 ];
 
 @NgModule({
@@ -19,4 +23,4 @@ const routes: Routes = [
   ],
   exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
